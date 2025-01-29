a489bd2156b991c67d5d24d3c1e65cf51fe6a1b8
gobro21 <sembohdan21@gmail.com>
2025-01-29 14:40:50 +0200

    oke

--- online_learning/calculator.py
+++ online_learning/calculator.py
@@ -6,21 +6,32 @@ def give_product(a, b):
     print("Результат:", a * b)
 def give_quotient(a, b):
     print("Результат:", a / b)
+def give_remainder(a, b):
+    print("Результат:", a % b)
+def exponentiate(a, c):
+    print("Результат:", a ** c)
 try:
-    num1 = float(input("Введіть перше число: "))
-    num2 = float(input("Введіть друге число: "))
-    operation = input("Виберіть операцію (+, -, * або /): ")
-    if operation == '+':
-        give_sum(num1, num2)
-    elif operation == '-':
-        give_difference(num1, num2)
-    elif operation == '*':
-        give_product(num1, num2)
-    elif operation == '/':
-        give_quotient(num1, num2)
+    operation = input("Виберіть операцію (+, -, *, /, % або **): ")
+    if operation in ('+', '-', '*', '/', '%'):
+        num1 = float(input("Введіть перше число: "))
+        num2 = float(input("Введіть друге число: "))
+        if operation == '+':
+            give_sum(num1, num2)
+        elif operation == '-':
+            give_difference(num1, num2)
+        elif operation == '*':
+            give_product(num1, num2)
+        elif operation == '/':
+            give_quotient(num1, num2)
+        elif operation == '%':
+            give_remainder(num1, num2)
+    elif operation == '**':
+        num1 = float(input("Введіть число: "))
+        num3 = float(input("Введіть степінь, в яку хочете піднести число: "))
+        exponentiate(num1, num3)
     else:
         print("Невідома операція")
 except ZeroDivisionError:
-    print('ділення на нуль неможливе')
+    print("Ділення на нуль неможливе")
 except ValueError:
-    print('введіть коректні цифри або числа')
+    print("Введіть коректні числа")
