fb1d1a8f0202a86d1f86ddaf2f6ed531fd224fb2
gobro21 <sembohdan21@gmail.com>
2025-01-29 12:30:57 +0200

    oke

--- online_learning/calculator.py
+++ online_learning/calculator.py
@@ -1,26 +1,25 @@
 def give_sum(a, b):
-    return a + b
+    print("Результат:", a + b)
 def give_difference(a, b):
-    return a - b
+    print("Результат:", a - b)
 def give_product(a, b):
-    return a * b
+    print("Результат:", a * b)
 def give_quotient(a, b):
-    return a / b
+    print("Результат:", a / b)
 try:
     num1 = float(input("Введіть перше число: "))
     num2 = float(input("Введіть друге число: "))
     operation = input("Виберіть операцію (+, -, * або /): ")
     if operation == '+':
-        result = give_sum(num1, num2)
+        give_sum(num1, num2)
     elif operation == '-':
-        result = give_difference(num1, num2)
+        give_difference(num1, num2)
     elif operation == '*':
-        result = give_product(num1, num2)
+        give_product(num1, num2)
     elif operation == '/':
-        result = give_quotient(num1, num2)
+        give_quotient(num1, num2)
     else:
         result = "Невідома операція"
-    print("Результат:", result)
 except ZeroDivisionError:
     print('ділення на нуль неможливе')
 except ValueError:
